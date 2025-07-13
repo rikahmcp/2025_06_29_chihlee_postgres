a463b497df1826d732ac768bbb4cfbad37f7c444
rikahmcp <rikahmcp@gmail.com>
2025-07-13 06:42:03 +0000

    3rd edit

---
+++ lesson5/lesson5_2.sql
@@ -0,0 +1,32 @@
+/*刪除舊表格 */
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS branch CASCADE;
+
+/*創立employee表格 */
+CREATE TABLE employee(
+	emp_id SERIAL,
+	name VARCHAR(20),
+	birth_date DATE,
+	sex VARCHAR(1),
+	salary INT,
+	branch_id INT,
+	sup_id INT,
+ 	PRIMARY KEY(emp_id)
+);
+
+/*創立branch表格 */
+CREATE TABLE branch(
+	branch_id INT,
+	branch_name VARCHAR(20),
+	manager_id INT,
+	PRIMARY KEY(branch_id),
+	FOREIGN KEY(manager_id) 
+	REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+/*補上employee少設的2個Foreign key */
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL;
+
+ALTER TABLE employee
+ADD FOREIGN KEY(sup_id) REFERENCES employee(emp_id) ON DELETE SET NULL;
---
+++ lesson6/lesson6_1.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "台鐵車站資訊" ADD primary KEY ("stationCode");
+
+ALTER TABLE "每日各站進出站人數" ADD FOREIGN KEY("車站代碼")
+REFERENCES "台鐵車站資訊"("stationCode");
+
+
+
